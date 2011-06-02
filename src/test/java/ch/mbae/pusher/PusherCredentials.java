5df66dd1e34dc311a4d78c97808ebb0d98325dd1
marc.baechinger@gmail.com <>
2011-06-02 18:02:25 +0200

    use your own account ;-)

--- src/test/java/ch/mbae/pusher/PusherCredentials.java
+++ src/test/java/ch/mbae/pusher/PusherCredentials.java
@@ -10,9 +10,9 @@ package ch.mbae.pusher;
  */
 public interface PusherCredentials {
 
-    static final String APPLICATION_ID = "5805";
-    static final String APPLICATION_KEY = "16f5c19c1e51b4e820e1";
-    static final String APPLICATION_SECRET = "b7ff31690e502636eac3";
+    static final String APPLICATION_ID = "";
+    static final String APPLICATION_KEY = "";
+    static final String APPLICATION_SECRET = "";
     
     static final String CHANNEL = "junit-test-channel";
     static final String EVENT = "junit-test-event";
